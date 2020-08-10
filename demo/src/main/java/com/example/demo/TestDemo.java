be3b98eebace5c9543b8072369b77faab027e8ea
ouhuikang <ouhuikang@highsoft.com>
2020-08-10 14:18:55 +0800

    my_branch2

--- demo/src/main/java/com/example/demo/TestDemo.java
+++ demo/src/main/java/com/example/demo/TestDemo.java
@@ -4,5 +4,6 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("my_branch");
+        System.out.println("my_branch2");
     }
 }
