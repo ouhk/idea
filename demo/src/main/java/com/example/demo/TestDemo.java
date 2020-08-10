07f06b2287163aadc830c248e6ce526b8dd059a2
ouhuikang <ouhuikang@highsoft.com>
2020-08-10 16:36:18 +0800

    my_branch5

--- demo/src/main/java/com/example/demo/TestDemo.java
+++ demo/src/main/java/com/example/demo/TestDemo.java
@@ -6,6 +6,6 @@ public class TestDemo {
         System.out.println("my_branch");
         System.out.println("my_branch2");
         System.out.println("my_branch4");
-        System.out.println("my_branch8");
+        System.out.println("my_branch5");
     }
 }
